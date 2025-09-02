600935b21b64ab6e95c45fe32c50279f7126e1da
jomskiee64 <guzmanjomel0@gmail.com>
2025-09-02 18:44:23 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/jomel', function () {
+    return view('jomel');
+});
+Route::get('/profile', function () {
+    return view('profile');
+});
+Route::get('/jomsresume', function () {
+    return view('jomsresume');
+});
+Route::get('/about us', function () {
+    return view('about us');
+});
